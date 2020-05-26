78599c2a878a6a66adbde9cc924545e39acf5cbc
Ciaran McCamphill <Ciaran.McCamphill@yahoo.com>
2020-05-26 15:51:20 +0100

    Added Website

---
+++ Scripts/app.js
@@ -0,0 +1,25 @@
+﻿
+$(function () {
+
+    $(window).on("load resize", function () {
+        $(".fill-screen").css("height", window.innerHeight);
+        $(".fill-screen").css("width", window.innerWidth);
+    });
+
+    //add bootstrap scrollspy
+    $('body').scrollspy({
+        target: '.navbar'
+    });
+
+
+    //smooth scrolling
+    $('nav a, .down-button a').bind('click', function () {
+        $('html, body').stop().animate({
+            scrollTop: $($(this).attr('href')).offset().top - 60
+    },1500,'easeInOutExpo');
+    event.preventDefault();
+    });
+
+    //Activate Wow.js
+    new WOW().init();
+});

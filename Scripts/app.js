98968b942bcb6056b26cb730970f7dc5dbb68980
Ciaran McCamphill <Ciaran.McCamphill@yahoo.com>
2020-05-26 17:12:01 +0100

    app js update

--- Scripts/app.js
+++ Scripts/app.js
@@ -6,20 +6,43 @@ $(function () {
         $(".fill-screen").css("width", window.innerWidth);
     });
 
-    //add bootstrap scrollspy
-    $('body').scrollspy({
-        target: '.navbar',
-        offset: 65
+
+    if ($(window).width() <= 800) {
+        //add bootstrap scrollspy
+        $('body').scrollspy({
+            target: '.navbar',
+            offset: 245
+        });
+
+        //smooth scrolling
+    $('nav a, .down-button a').bind('click', function () {
+        $('html, body').stop().animate({
+        scrollTop: $($(this).attr('href')).offset().top -235
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
     });
+    }
+    else
+    {
+        //add bootstrap scrollspy
+        $('body').scrollspy({
+            target: '.navbar',
+            offset: 65
+        });
 
-
-    //smooth scrolling
+        //smooth scrolling
     $('nav a, .down-button a').bind('click', function () {
         $('html, body').stop().animate({
             scrollTop: $($(this).attr('href')).offset().top - 60
     },1500,'easeInOutExpo');
     event.preventDefault();
     });
+   }
+
+    
+
+
+    
 
     //Activate Wow.js
     new WOW().init();
